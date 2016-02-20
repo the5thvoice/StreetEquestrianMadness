1e6dd9025e977280065c51df67928331d57680da
tanya byrne <tanya.jo.byrne@gmail.com>
2016-02-20 11:51:34 +0000

    forward and reverse movemnt

---
+++ Assets/Scripts/SEM_CharacterController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SEM_CharacterController : MonoBehaviour {
+
+    public List<float> MaxSpeeds;
+    Rigidbody _rb;
+    Rigidbody rb
+    {
+        get
+        {
+            if (_rb == null)
+                _rb = GetComponent<Rigidbody>();
+            return _rb;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float speed = Input.GetAxis("Vertical") * MaxSpeeds[0];
+
+        rb.AddForce(transform.forward * speed);
+            
+	
+	}
+}
