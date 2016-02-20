bf00a95013ed2ec30ce4a2cb7bbeb4b165938179
tanya byrne <tanya.jo.byrne@gmail.com>
2016-02-20 14:41:02 +0000

    control and steerring

--- Assets/Scripts/SEM_CharacterController.cs
+++ Assets/Scripts/SEM_CharacterController.cs
@@ -1,10 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class SEM_CharacterController : MonoBehaviour {
 
     public List<float> MaxSpeeds;
+    public int Gear = 1;
+    public float AccelerationRate;
+    private float CurrentSpeed = 0f;
+    public float Speed
+    {
+        get
+        {
+            float direction = Input.GetAxis("Vertical");
+
+            
+            if (Mathf.Abs(CurrentSpeed) >= MaxSpeeds[Gear - 1])
+                return direction * MaxSpeeds[Gear - 1];
+            else
+            {
+                CurrentSpeed += AccelerationRate;
+                return direction * CurrentSpeed;
+            }
+           
+           
+
+            
+        }
+    }
+
+    public float TurningSpeed;
+
+    
     Rigidbody _rb;
     Rigidbody rb
     {
@@ -16,18 +44,25 @@ public class SEM_CharacterController : MonoBehaviour {
         }
     }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        float speed = Input.GetAxis("Vertical") * MaxSpeeds[0];
+        
 
-        rb.AddForce(transform.forward * speed);
-            
-	
-	}
+        MoveForevard();
+
+        float rotation = Input.GetAxis("Horizontal") * TurningSpeed;
+        transform.Rotate(0, rotation, 0);
+
+
+
+    }
+
+    private void MoveForevard()
+    {
+        
+
+        rb.AddForce(transform.forward * Speed);
+    }
 }
