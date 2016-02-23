941d1adbad3ce1d85c487c68c516adb6a3b173cb
tanya byrne <tanya.jo.byrne@gmail.com>
2016-02-23 09:56:27 +0000

    Obsticles & Collision Detection

---
+++ Assets/Scripts/SEM_InteractableObject.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public enum ObjectType
+{
+    collectable,
+    DestructableBarrier,
+    FixedBarrier,
+}
+
+public class SEM_InteractableObject : MonoBehaviour
+{
+
+    public ObjectType ItemType;
+
+    Rigidbody RB;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Setup();
+
+    }
+
+    private void Setup()
+    {
+        switch (ItemType)
+        {
+
+            case ObjectType.collectable:
+
+                return;
+            case ObjectType.DestructableBarrier:
+                AddRB(0.1f);
+                return;
+            case ObjectType.FixedBarrier:
+                AddRB(1);
+                RB.constraints = RigidbodyConstraints.FreezeAll;
+                return;
+
+        }
+    }
+
+    private void AddRB(float Mass)
+    {
+        gameObject.AddComponent<Rigidbody>();
+        RB = GetComponent<Rigidbody>();
+        RB.mass = Mass;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+
+        switch (ItemType)
+        {
+
+            case ObjectType.collectable:
+                Destroy(gameObject);
+                return;
+                //todo: damage application when/if helath system implemented
+
+        }
+
+
+
+    }
+}
