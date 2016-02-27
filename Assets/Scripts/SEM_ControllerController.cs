04dc403863efa2af1ac86f993cec290d3480b483
tanya byrne <tanya.jo.byrne@gmail.com>
2016-02-27 13:09:25 +0000

    splitscreen and controls controller

---
+++ Assets/Scripts/SEM_ControllerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Players
+{
+    PlayerOne,
+    PlayerTwo,
+}
+
+public class SEM_ControllerController : MonoBehaviour {
+
+    public static string Horizantal(Players p, bool Keyboard)
+    {
+        switch (p)
+        {
+            default:
+            case Players.PlayerOne:
+                if (Keyboard)
+                    return "PlayerOneHorizontalPad";
+                else
+                    return "PlayerOneHorizontalKeyboard";
+            case Players.PlayerTwo:
+                return "PlayerTwoHorizontal";
+
+        }
+    }
+
+    public static string Accelerator(Players p, bool Keyboard)
+    {
+        switch (p)
+        {
+            default:
+            case Players.PlayerOne:
+                if (Keyboard)
+                    return "PlayerOneAccelPad";
+                else
+                    return "PlayerOneAccelKeyboard";
+            case Players.PlayerTwo:
+                return "PlayerTwoAccel";
+
+        }
+    }
+
+
+    public static string GearShift(Players p, bool Keyboard)
+    {
+        switch (p)
+        {
+            default:
+            case Players.PlayerOne:
+                if (Keyboard)
+                    return "PlayerOneGearShiftPad";
+                else
+                    return "PlayerOneGearShiftKeyboard";
+            case Players.PlayerTwo:
+                return "PlayerTwoGearShift";
+
+        }
+    }
+
+
+}
