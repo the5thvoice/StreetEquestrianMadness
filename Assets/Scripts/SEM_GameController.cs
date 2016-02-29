1324a6b1185ec1b4eb68420981358cee981a2dfa
tanya byrne <tanya.jo.byrne@gmail.com>
2016-02-29 14:57:46 +0000

    InGameMenuStart

---
+++ Assets/Scripts/SEM_GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SEM_GameController : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        PlayPause();
+	
+	}
+
+    private void PlayPause()
+    {
+        throw new NotImplementedException();
+    }
+}
