5899856f51fbbe9dad625123562bf9270968aad2
tanya byrne <tanya.jo.byrne@gmail.com>
2016-04-25 11:41:38 +0100

    music

---
+++ Assets/Scripts/SEM_Music.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SEM_Music : MonoBehaviour
+{
+
+    public List<AudioClip> Tracks;
+    public AudioSource MusicPlayer;
+   
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	    int randomT = Random.Range(0, Tracks.Count - 1);
+
+	    MusicPlayer.clip = Tracks[randomT];
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    if (MusicPlayer.isPlaying)
+	        return;
+
+        int randomT = Random.Range(0, Tracks.Count - 1);
+
+        while(MusicPlayer.clip != Tracks[randomT])
+            randomT = Random.Range(0, Tracks.Count - 1);
+
+        MusicPlayer.clip = Tracks[randomT];
+	    MusicPlayer.Play();
+
+	}
+}
